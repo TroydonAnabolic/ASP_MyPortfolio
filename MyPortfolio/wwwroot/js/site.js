fffe473c9069d0ab954b9e53554c515c2d755b20
Troydon Luicien <troyincarnate@gmail.com>
2020-02-25 08:10:04 +1100

    Add project files.

---
+++ MyPortfolio/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+
+// Open and close gate animation
+$(".open_close_doors").click(function () {
+    // move the left and right gate-closed to outwards
+    $("#leftdoor_inner").animate({ "left": "-=50px" }, "slow");
+    $("#rightdoor_inner").animate({ "left": "+=50px" }, "slow");
+    // once doors moved outwards then deactivate the gate closs class and reveal open gate class
+    // Also show profile About me Info
+});
