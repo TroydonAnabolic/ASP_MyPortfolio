c77617dd6dc6de2effa1a10ea93f5044f73dc4ae
Troydon Luicien <troyincarnate@gmail.com>
2020-02-27 23:20:51 +1100

    Fixed up text link to large so it is more prevalent.

--- MyPortfolio/wwwroot/js/site.js
+++ MyPortfolio/wwwroot/js/site.js
@@ -7,8 +7,23 @@ $(".open_close_doors").click(function () {
     // move the left and right gate-closed to outwards
     $("#leftdoor_inner").animate({ "left": "-=125px" }, "slow");
     $("#rightdoor_inner").animate({ "left": "+=125px" }, "slow");
-    $("#leftdoor_outer").animate({ "left": "-=500px" }, "slow");
-    $("#rightdoor_outer").animate({ "left": "+=500px" }, "slow");
-    // once doors moved outwards then deactivate the gate closs class and reveal open gate class
-    // Also show profile About me Info
+    $("#leftdoor_outer").animate({ "left": "-=1000px" }, "slow");
+    $("#rightdoor_outer").animate({ "left": "+=1000px" }, "slow");
 });
+
+// once doors moved outwards then deactivate the gate closs class and reveal open gate class
+    // Also show profile About me Info
+
+// assign co-ordinates of the closed gate after it's class has been removed
+var right_close = document.getElementById('rightdoor_inner');
+var right_open = document.getElementById('rightdoor_open');
+
+right_open.style.position = "absolute";
+right_open.style.left = x_pos + 'px';
+right_open.style.top = y_pos + 'px';
+
+var left_close = document.getElementById('leftdoor_inner');
+var left_open = document.getElementById('leftdoor_open');
+left_open .style.position = "absolute";
+left_open .style.left = x_pos + 'px';
+left_open .style.top = y_pos + 'px';
