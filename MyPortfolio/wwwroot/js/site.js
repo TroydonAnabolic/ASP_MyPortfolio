f5ac9bbd5350d33505cc3361ee7c1ccb7fd7a6a6
Troydon Luicien <troyincarnate@gmail.com>
2020-02-28 00:09:10 +1100

    Enabled timer for page to re-direct to about me page on click. Also added add class on click which will appear for 2 seconds only. To do: enable class to either inherit current position of the gates that moved or manually set the new values.

--- MyPortfolio/wwwroot/js/site.js
+++ MyPortfolio/wwwroot/js/site.js
@@ -4,26 +4,44 @@
 
 // Open and close gate animation
 $(".open_close_doors").click(function () {
+
+    // follow link after 6 seconds TODO: change link to BIO/About me instead of privacy after test
+    $(function () {
+        setTimeout(function () {
+            window.location.href = "/Home/Privacy";
+        }, 6000);
+    });
+
     // move the left and right gate-closed to outwards
-    $("#leftdoor_inner").animate({ "left": "-=125px" }, "slow");
-    $("#rightdoor_inner").animate({ "left": "+=125px" }, "slow");
-    $("#leftdoor_outer").animate({ "left": "-=1000px" }, "slow");
-    $("#rightdoor_outer").animate({ "left": "+=1000px" }, "slow");
+    $("#leftdoor_inner").animate({ "left": "-=15%" }, "slow");
+    $("#rightdoor_inner").animate({ "left": "+=15%" }, "slow");
+    $("#leftdoor_outer").animate({ "left": "-=100%" }, "slow");
+    $("#rightdoor_outer").animate({ "left": "+=100%" }, "slow");
+    // remove the closed gates from display
+    $(".open_close_doors").addClass("hidden");
+
+    // add the hidden class to the closed doors image
+    $("#rightdoor_open").addClass("rightdoor_open").delay(2000).queue(function (next) {
+        $(this).removeClass("rightdoor_open");
+        next();
+    });
 });
+
+
 
 // once doors moved outwards then deactivate the gate closs class and reveal open gate class
     // Also show profile About me Info
 
 // assign co-ordinates of the closed gate after it's class has been removed
 var right_close = document.getElementById('rightdoor_inner');
-var right_open = document.getElementById('rightdoor_open');
+var right_open = document.getElementsByClassName('rightdoor_open');
 
 right_open.style.position = "absolute";
 right_open.style.left = x_pos + 'px';
 right_open.style.top = y_pos + 'px';
 
 var left_close = document.getElementById('leftdoor_inner');
-var left_open = document.getElementById('leftdoor_open');
+var left_open = document.getElementsByClassName('leftdoor_open');
 left_open .style.position = "absolute";
 left_open .style.left = x_pos + 'px';
 left_open .style.top = y_pos + 'px';
