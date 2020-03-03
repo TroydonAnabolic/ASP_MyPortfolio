50e01c56528b40441c2cb88d30e6c3a56c119722
Troydon Luicien <troyincarnate@gmail.com>
2020-03-03 16:45:24 +1100

    removed commented out test code.

--- MyPortfolio/wwwroot/js/pre-loader.js
+++ MyPortfolio/wwwroot/js/pre-loader.js
@@ -1,16 +1,4 @@
-﻿//$(window).on('load', function () {
-
-//    // add text
-//    $('#pre-loader').each(function () {
-//        $(this).before($('<span>').text("Loading..."));
-//    });
-
-//    $("#preloader").addClass(".loader");
-//    $(".loader").fadeOut("slow");
-
-//}); #262626;
-
-
+﻿
     $(document).ready(function() {
         //Preloader
         preloaderFadeOutTime = 3000;
