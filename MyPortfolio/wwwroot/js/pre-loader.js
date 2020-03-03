0cef517ceb6286b76202dd40189bfea6d5237076
Troydon Luicien <troyincarnate@gmail.com>
2020-03-03 12:00:24 +1100

    Added in pre-loader, attempting to ensure it only shows during loading.

---
+++ MyPortfolio/wwwroot/js/pre-loader.js
@@ -0,0 +1,10 @@
+﻿$(window).on('load', function () {
+
+    // add text
+    $('#pre-loader').each(function () {
+        $(this).before($('<span>').text("Loading..."));
+    });
+
+    $("#preloader").addClass(".loader");
+    $(".loader").fadeOut("slow");
+});
